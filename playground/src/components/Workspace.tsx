909365c2ca18502e12e21f24b230659d703725a0
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-31 23:36:10 +0100

    The playground works now!

--- playground/src/components/Workspace.tsx
+++ playground/src/components/Workspace.tsx
@@ -1,8 +1,22 @@
-import { useMemo } from "voby"
+import { $, useMemo } from "voby"
 import { Split } from "./Split"
 import { viewportWidth } from "../appState"
+import { Interpreter } from "@mmk21/c"
 
 export default function Workspace() {
+  const outputString = $<string | null>(null)
+
+  function runCode() {
+    const codeInput = document.getElementById(
+      "code-input"
+    ) as HTMLTextAreaElement | null
+    if (!codeInput) throw new Error("Code input element not found")
+    const code = codeInput.value
+    const interpreter = new Interpreter()
+    const exceptions = interpreter.run(code)
+    outputString(interpreter.boardToFormattedString())
+  }
+
   return (
     <Split.Provider>
       <div
@@ -13,14 +27,21 @@ export default function Workspace() {
           ),
         }}
       >
-        <div class="py-2 px-2">
+        <div class="py-2 px-2 flex flex-col">
           <textarea
-            class="textarea w-full"
+            class="textarea w-full h-full"
+            id="code-input"
             placeholder="Start writing some code..."
           ></textarea>
+          <div class="flex justify-end">
+            <button class="btn mt-2" onClick={runCode}>
+              Run code
+            </button>
+          </div>
         </div>
         <div class="py-2 px-2">
-          <h2>Output</h2>
+          <h2 class="font-semibold text-xl mb-2">Output</h2>
+          <pre class="overflow-auto leading-none py-2">{outputString}</pre>
         </div>
       </div>
     </Split.Provider>
