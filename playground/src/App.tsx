de4cab373a52961656d29f0d8b9ccbc491198ed2
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-07 16:50:24 +0100

    Add voby-simple-router

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,15 +1,24 @@
 import { $ } from "voby"
 import Workspace from "./components/Workspace"
 import { TopAppBar } from "./components/TopAppBar"
+import { Route, Router, RouterRoute } from "voby-simple-router"
+import Playground from "./pages/Playground"
+
+const routes: RouterRoute[] = [
+  {
+    path: "/playground",
+    to: Playground,
+  },
+]
 
 function App(): JSX.Element {
   return (
-    <div class="h-[100dvh] flex flex-col">
-      <TopAppBar />
-      <main class="header flex-center text-center flex-1">
-        <Workspace />
-      </main>
-    </div>
+    <Router routes={routes}>
+      <div class="h-[100dvh] flex flex-col">
+        <TopAppBar />
+        <Route />
+      </div>
+    </Router>
   )
 }
 
